6fbaac24ef2a1cbf499af823946ab73e0a7fc2b9
Yasmin <yasmin@MacBook-Air-Yasmin.local>
2026-08-12 23:51:47 +0700

    квест

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    char secret[]={
+        66,
+        79,
+        79,
+        75,
+        83,
+        72,
+        69,
+        76,
+        70,
+        0
+    }
+
+    prinf("%s",secret);
+
+    return 0;
+}
